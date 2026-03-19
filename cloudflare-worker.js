1967374c747f502a1971f28f2542966068e44cd1
dlgksxk <dlgksxk@gmail.com>
2026-03-19 09:56:15 +0000

    Handle SPA refresh routes

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -3,6 +3,9 @@ export default {
     const url = new URL(request.url);
     const isApiRequest = url.pathname.startsWith('/api/');
     const isLegacyTryOnRequest = url.pathname === '/generateTryOn';
+    const acceptsHtml = request.headers.get('accept')?.includes('text/html') ?? false;
+    const isNavigationRequest = (request.method === 'GET' || request.method === 'HEAD') && acceptsHtml;
+    const isStaticAssetRequest = /\.[a-z0-9]+$/i.test(url.pathname);
 
     // Preflight handling
     if (request.method === 'OPTIONS') {
@@ -24,8 +27,22 @@ export default {
       targetUrl.pathname = url.pathname;
     } else if (isLegacyTryOnRequest) {
       targetUrl.hostname = 'asia-northeast3-hamdeva.cloudfunctions.net';
+    } else if (env.ASSETS) {
+      const assetResponse = await env.ASSETS.fetch(request);
+      if (assetResponse.status !== 404 || isStaticAssetRequest || !isNavigationRequest) {
+        return assetResponse;
+      }
+
+      const fallbackUrl = new URL(request.url);
+      fallbackUrl.pathname = '/index.html';
+      fallbackUrl.search = '';
+      return env.ASSETS.fetch(new Request(fallbackUrl.toString(), request));
     } else {
       targetUrl.hostname = 'hamdeva.web.app';
+      if (!isStaticAssetRequest && isNavigationRequest) {
+        targetUrl.pathname = '/';
+        targetUrl.search = '';
+      }
     }
 
     console.log('[HAMDEVA-worker] proxy request', {
